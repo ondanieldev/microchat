c9c22dd810959b48b85d9c0bf8736dd1c124d3ad
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2021-07-29 22:44:42 -0300

    feat(database): setup typeorm

---
+++ backend/src/Shared/Infra/TypeORM/Migrations/1627609171860-CreateTableJoins.ts
@@ -0,0 +1,83 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateTableJoins1627609171860
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'joins',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'room_id',
+            type: 'uuid',
+          },
+          {
+            name: 'is_moderator',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'joins',
+      new TableForeignKey({
+        name: 'joins_user_id',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'joins',
+      new TableForeignKey({
+        name: 'joins_room_id',
+        columnNames: ['room_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'rooms',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('joins', 'joins_room_id');
+
+    await queryRunner.dropForeignKey('joins', 'joins_user_id');
+
+    await queryRunner.dropTable('joins');
+  }
+}
