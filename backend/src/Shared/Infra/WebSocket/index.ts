53ce8b180095b3a5deaf1c5403127e1b21a4e9bc
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2021-08-22 23:01:45 -0300

    feat(sockets): setup events and auth

--- backend/src/Shared/Infra/WebSocket/index.ts
+++ backend/src/Shared/Infra/WebSocket/index.ts
@@ -1,6 +1,8 @@
 import socketIO from 'socket.io';
 import { Server } from 'http';
 import { Handler, Request, Response, NextFunction } from 'express';
+import AuthenticateUser from 'Modules/Users/Services/AuthenticateUser';
+import { container } from 'tsyringe';
 
 class WebSocket {
   private server: socketIO.Server;
@@ -10,9 +12,28 @@ class WebSocket {
       serveClient: false,
     });
 
-    this.server.on('connection', socket => {
-      console.log('a');
+    this.server.on('connection', async socket => {
+      const { token } = socket.handshake.auth;
+
+      const authenticateUser = container.resolve(AuthenticateUser);
+      try {
+        await authenticateUser.execute(token);
+      } catch (err) {
+        socket.emit('error', err);
+        return;
+      }
       socket.emit('connection');
+
+      socket.on('room.join', data => {
+        if (!data || !data.room_id) {
+          socket.emit('error', {
+            message: 'Romm not found!',
+            statusCode: 404,
+          });
+          return;
+        }
+        socket.join(data.room_id);
+      });
     });
   }
 
